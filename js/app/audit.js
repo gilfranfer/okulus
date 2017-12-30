f53b0ebeb208b5d10bbb72364ceadbd23d874ccf
Fernando Gil <gil.franfer@gmail.com>
2017-12-30 10:52:56 -0600

    Audit Service Documentation

--- js/app/audit.js
+++ js/app/audit.js
@@ -1,23 +1,28 @@
 
-okulusApp.factory('AuditSvc', ['$rootScope',  
+okulusApp.factory('AuditSvc', ['$rootScope',
 	function($rootScope){
 		let baseRef = firebase.database().ref().child('pibxalapa');
-	    
+
 		return {
+			/**
+			 * This method will receive the reference of the updated Db record and will
+			 * use its Id to create a couple of Audit records:
+			 * 1. Record in the object itself (actions: creation,update)
+			 * 2. Record in the App Global Audit Folder. (actions: creation,update, delete)
+			 */
 			recordAudit: function( ref, action, on){
 				let audit = {action: action, by: "admin", date: firebase.database.ServerValue.TIMESTAMP};
-				
+
 				if(action === 'delete'){
-					//On delete there is no need to stor this in the deleted object
-				}else{					
+					//On delete there is no need to create an Audit in the object, because it will be removed
+				}else{
 				    //Audit in the object itself
 				    ref.child("audit").push().set(audit);
 				}
-			    
-			    //global audit
-			    audit.on = on;
-			    audit.id = ref.key;
-			    baseRef.child("audit").push().set(audit);
+		    //app global audit
+		    audit.on = on;
+		    audit.id = ref.key;
+		    baseRef.child("audit").push().set(audit);
 			}
 		};
 	}
