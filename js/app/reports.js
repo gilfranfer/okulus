46c1ac9f7b86b7bd5f60075a00aba79ce33b62c0
Franfer Gil <gil.franfer@gmail.com>
2018-01-24 23:33:15 -0600

    Attendance Default Values

--- js/app/reports.js
+++ js/app/reports.js
@@ -1,8 +1,22 @@
-okulusApp.controller('ReportCntrl', ['$rootScope', 'GroupsSvc', 'MembersSvc',
-	function($rootScope, GroupsSvc, MembersSvc){
+okulusApp.controller('ReportCntrl', ['$rootScope','$scope', 'GroupsSvc', 'MembersSvc',
+	function($rootScope, $scope, GroupsSvc, MembersSvc){
 		
 		GroupsSvc.loadActiveGroups();
 		MembersSvc.loadActiveMembers();
+
+		//To put default Values on Attendance
+		$scope.report = {
+			attendance:{
+				guests:{
+					male:{kid:0, young:0, adult:0},
+					female:{kid:0, young:0, adult:0}
+				},
+				members:{
+					male:{kid:0, young:0, adult:0},
+					female:{kid:0, young:0, adult:0}
+				}
+			}
+		};
 
 	}
 ]);
