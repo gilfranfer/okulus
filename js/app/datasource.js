5f7d597cb9105de8595d1978678cd0592eebc898
Fernando Gil <gil.franfer@gmail.com>
2018-04-23 23:13:35 -0500

    Datasource

--- js/app/datasource.js
+++ js/app/datasource.js
@@ -1,20 +1,10 @@
 // Initialize Firebase
   var config = {
-    /* Dev Sandbox */
     apiKey: "AIzaSyAERYbnjq6Hu3gFFNBptRvbcvlBt4anwHk",
     authDomain: "dev-sandbox-b557a.firebaseapp.com",
     databaseURL: "https://dev-sandbox-b557a.firebaseio.com",
     projectId: "dev-sandbox-b557a",
     storageBucket: "dev-sandbox-b557a.appspot.com",
     messagingSenderId: "962638064654"
-
-    /* Prod
-    apiKey: "AIzaSyA-fKa3EmMg_t_YOTzhOf36JhKUCyCBS1A",
-    authDomain: "okulus-prd.firebaseapp.com",
-    databaseURL: "https://okulus-prd.firebaseio.com",
-    projectId: "okulus-prd",
-    storageBucket: "",
-    messagingSenderId: "681782068973"
-    */
   };
   firebase.initializeApp(config);
