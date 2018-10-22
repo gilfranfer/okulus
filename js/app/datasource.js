be9b2c12d08bed497ea612b0953268ef45c3a76c
Fernando Gil <gil.franfer@gmail.com>
2018-10-22 09:41:40 -0500

    datasource update

--- js/app/datasource.js
+++ js/app/datasource.js
@@ -1,20 +1,10 @@
-// Initialize Firebase
-  const rootFolder = "okulus";
-  var config = {
-    apiKey: "AIzaSyAERYbnjq6Hu3gFFNBptRvbcvlBt4anwHk",
-    authDomain: "dev-sandbox-b557a.firebaseapp.com",
-    databaseURL: "https://dev-sandbox-b557a.firebaseio.com",
-    projectId: "dev-sandbox-b557a",
-    storageBucket: "dev-sandbox-b557a.appspot.com",
-    messagingSenderId: "962638064654"
-  };
-  // const rootFolder = "pibxalapa";
-  // var config = {
-  //   apiKey: "AIzaSyA-fKa3EmMg_t_YOTzhOf36JhKUCyCBS1A",
-  //   authDomain: "okulus-prd.firebaseapp.com",
-  //   databaseURL: "https://okulus-prd.firebaseio.com",
-  //   projectId: "okulus-prd",
-  //   storageBucket: "okulus-prd.appspot.com",
-  //   messagingSenderId: "681782068973"
-  // };
-  firebase.initializeApp(config);
+const rootFolder = "okulus";
+var config = {
+  apiKey: "AIzaSyAERYbnjq6Hu3gFFNBptRvbcvlBt4anwHk",
+  authDomain: "dev-sandbox-b557a.firebaseapp.com",
+  databaseURL: "https://dev-sandbox-b557a.firebaseio.com",
+  projectId: "dev-sandbox-b557a",
+  storageBucket: "dev-sandbox-b557a.appspot.com",
+  messagingSenderId: "962638064654"
+};
+firebase.initializeApp(config);
