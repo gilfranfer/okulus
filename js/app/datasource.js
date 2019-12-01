d28be61c96bee1c1ba73f8c8d151661f86a00c26
Fernando Gil <gil.franfer@gmail.com>
2019-12-01 11:39:33 -0600

    Okulus Dev datasource

--- js/app/datasource.js
+++ js/app/datasource.js
@@ -1,32 +1,13 @@
-//Okulust Test
-// var config = {
-//   apiKey: "AIzaSyAERYbnjq6Hu3gFFNBptRvbcvlBt4anwHk",
-//   authDomain: "dev-sandbox-b557a.firebaseapp.com",
-//   databaseURL: "https://dev-sandbox-b557a.firebaseio.com",
-//   projectId: "dev-sandbox-b557a",
-//   storageBucket: "dev-sandbox-b557a.appspot.com",
-//   messagingSenderId: "962638064654"
-// };
+//Okulus Realtime Database configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDoBp367ZPXt6upXCDZQY0KhSjJpGXkkUI",
+  authDomain: "okulusdev.firebaseapp.com",
+  databaseURL: "https://okulusdev.firebaseio.com",
+  projectId: "okulusdev",
+  storageBucket: "okulusdev.appspot.com",
+  messagingSenderId: "199609968467",
+  appId: "1:199609968467:web:b6673844f858fe88c363a6",
+  measurementId: "G-CYPF5CB8TP"
+};
 
-// OHC Dev
-// var config = {
-//   apiKey: "AIzaSyCbZUIZjlz87NFJnoyet52egjJU4GnqqfY",
-//   authDomain: "okulus-ohc-dev.firebaseapp.com",
-//   databaseURL: "https://okulus-ohc-dev.firebaseio.com",
-//   projectId: "okulus-ohc-dev",
-//   storageBucket: "okulus-ohc-dev.appspot.com",
-//   messagingSenderId: "478849109524"
-// };
-
-//Okulus QA
-var config = {
-    apiKey: "AIzaSyAwpfABRHeN7G5YQre0SKFSNUJUCujrEQc",
-    authDomain: "okulus-ohc-qa.firebaseapp.com",
-    databaseURL: "https://okulus-ohc-qa.firebaseio.com",
-    projectId: "okulus-ohc-qa",
-    storageBucket: "",
-    messagingSenderId: "490528923430",
-    appId: "1:490528923430:web:b423fbb55d14c349"
-  };
-
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
