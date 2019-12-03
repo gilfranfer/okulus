a5d89fd078f09af3b1942aed8e84bc000fbcb4bc
Fernando Gil <gil.franfer@gmail.com>
2019-12-02 21:00:21 -0600

    Lbl Fix for Duration chart

--- js/app/i18n.js
+++ js/app/i18n.js
@@ -672,7 +672,7 @@ okulusApp.run(function($rootScope) {
 				attendanceTitle:"Personas Ministradas",
 				reportsAndReunionsTitle:"Reportes y Reuniones",
 				durationTitle:"Minutos Ministrados", durationAvgTitle:"Promedios",
-				durationLbl:"Duración", durationAvgReunionLbl:"Promedio por Reunión", durationAvgAttnLbl:"Promedio por Reunión",
+				durationLbl:"Duración", durationAvgReunionLbl:"Promedio por Reunión", durationAvgAttnLbl:"Promedio por Asistencia",
 				moneyTitle:"Ofrenda",	moneyAvgReunionTitle:"Promedio por Reunión", moneyAvgAttnTitle:"Promedio por Asistencia",
 				activeSerieLbl:"Miembros Activos", inactiveSerieLbl:"Miembros Inactivos"
 			},
