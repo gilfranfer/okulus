a3ee87c17de953d863986039b40c3ceed42d3ddf
Franfer Gil <gil.franfer@gmail.com>
2019-12-08 10:48:45 -0600

    Update datasource.js

--- js/app/datasource.js
+++ js/app/datasource.js
@@ -1,11 +1,11 @@
 const rootFolder = "pibxalapa";
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyA-fKa3EmMg_t_YOTzhOf36JhKUCyCBS1A",
-    authDomain: "okulus-prd.firebaseapp.com",
-    databaseURL: "https://okulus-prd.firebaseio.com",
-    projectId: "okulus-prd",
-    storageBucket: "okulus-prd.appspot.com",
-    messagingSenderId: "681782068973"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   };
   firebase.initializeApp(config);
